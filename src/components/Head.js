4da0a074f0525fcc633b110f584a00007c334120
Varun Halhalli <111651992+hvarun13@users.noreply.github.com>
2023-08-08 20:26:17 +0530

    first commit

---
+++ src/components/Head.js
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+import { TfiSearch } from "react-icons/tfi";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+
+const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggestions(), 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    // console.log(json[1]);
+  };
+
+  const dispatch = useDispatch();
+
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg ">
+      <div className="flex col-span-1">
+        <img
+          onClick={() => toggleMenuHandler()}
+          className="h-6 cursor-pointer"
+          alt="menu"
+          src="https://icon-library.com/images/hamburger-menu-icon-png/hamburger-menu-icon-png-11.jpg"
+        />
+        <a href="/">
+          <img
+            className="h-6 mx-2"
+            alt="youtube-logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+          />
+        </a>
+      </div>
+      <div className="col-span-10">
+        <div className="  pl-48 flex">
+          <input
+            placeholder="Search"
+            className="w-1/2 border border-gray-400 p-1 pl-5 rounded-l-full "
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 py-1 px-6 rounded-r-full bg-gray-100">
+            <TfiSearch />
+          </button>
+        </div>
+
+        {showSuggestions && (
+          <div
+            className="absolute bg-white ml-48 py-2 px-1 w-[30rem] shadow-lg 
+          rounded-lg border border-gray-100"
+          >
+            <ul>
+              {suggestions.map((s) => (
+                <li
+                  key={s}
+                  className="flex items-center py-2 px-2 shadow-sm hover:bg-gray-100"
+                >
+                  <TfiSearch size={15} /> <span className="pl-3">{s}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className="col-span-1">
+        <img
+          className="h-6"
+          alt="user"
+          src="https://freesvg.org/img/abstract-user-flat-1.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
---
+++ src/components/VideoContainer.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
+
+const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    setVideos(json.items);
+  };
+
+  return (
+    <div className="flex flex-wrap ">
+      {videos.map((movieData) => (
+        <Link key={movieData.id} to={`/watch?v=${movieData.id}`}>
+          <VideoCard info={movieData} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
